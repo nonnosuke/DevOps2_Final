b2fce59cf45b379108d1929d0ac638c2a09000a3
nonnosuke <168043854+nonnosuke@users.noreply.github.com>
2025-10-28 21:36:06 -0700

    Update test_form.js

--- selenium_tests/test_form.js
+++ selenium_tests/test_form.js
@@ -16,7 +16,7 @@ const chrome = require('selenium-webdriver/chrome');
 
     //Initial page
     // Wait for modal buttons to appear
-    await driver.wait(until.elementLocated(By.xpath('//*[@id="r0"]')), 3000);
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="optionsDlg"]/div')), 5000);
     await driver.findElement(By.xpath('//*[@id="r0"]')).click();
 
     await driver.wait(until.elementLocated(By.xpath('//*[@id="rx"]')), 3000);
