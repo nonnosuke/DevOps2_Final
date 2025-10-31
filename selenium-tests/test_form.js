97aff7460cca97ccb4698a16d65bb066097823a1
nonnosuke <168043854+nonnosuke@users.noreply.github.com>
2025-10-30 19:02:45 -0700

    Update test_form.js

--- selenium-tests/test_form.js
+++ selenium-tests/test_form.js
@@ -13,7 +13,7 @@ const chrome = require('selenium-webdriver/chrome');
   let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
   try {
     await driver.get('http://54.175.235.187/index.html');
-    await driver.wait(until.elementIsVisible(By.id("optionsDlg")), 15000);
+    await driver.wait(until.elementLocated(By.id("optionsDlg")), 15000);
 
     //await driver.sleep(5000);
 
