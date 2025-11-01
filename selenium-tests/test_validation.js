db13ae7de68b01ec55fffb5b21c4946d4f9fd42b
nonnosuke <168043854+nonnosuke@users.noreply.github.com>
2025-10-31 17:11:53 -0700

    Update test_validation.js

--- selenium-tests/test_validation.js
+++ selenium-tests/test_validation.js
@@ -5,25 +5,17 @@ const chrome = require('selenium-webdriver/chrome');
 async function testTicTacToe() {
   // Create Chrome options
     let options = new chrome.Options();
-    options.addArguments('--headless');              // run without GUI
-    options.addArguments('--no-sandbox');            // needed for EC2
-    options.addArguments('--disable-dev-shm-usage'); // avoid shared memory issue
-    options.addArguments('--user-data-dir=/tmp/chrome-user-data-' + Date.now()); // unique profile
+    options.addArguments('headless');
+    options.addArguments('disable-gpu')
+    options.setChromeBinaryPath('/usr/bin/google-chrome');
   
   let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
   try {
     await driver.get('http://54.175.235.187/index.html');
-    await driver.sleep(5000);
+    await driver.sleep(3000);
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="optionsDlg"]/div')), 5000);
 
      try {
-      // --- Select easy ---
-        const easyRadio = await driver.findElement(By.id('r0'));
-        await easyRadio.click();
-
-      // --- Select player (X) ---
-      const xRadio = await driver.findElement(By.id('rx'));
-      await xRadio.click();
-
       // --- Click Play button ---
       const playButton = await driver.findElement(By.id('okBtn'));
       await playButton.click();
@@ -36,7 +28,6 @@ async function testTicTacToe() {
     // Click the first cell
     const cell = await driver.findElement(By.id("cell0"));
     await cell.click();
-
     
     // Intentionally wait for wrong text to force failure
     await driver.wait(until.elementTextContains(cell, 'O'), 3000);
